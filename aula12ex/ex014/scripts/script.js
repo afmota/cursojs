859fec0efcb04b7e18645cc492163b88f4b28b94
AGNALDO FRANCISCO DA MOTA <af_mota@hotmail.com>
2023-12-16 16:39:56 -0300

    aula 13

--- aula12ex/ex014/scripts/script.js
+++ aula12ex/ex014/scripts/script.js
@@ -9,12 +9,15 @@ function carregar() {
     if (hora >= 0 && hora < 12) {
         // bom dia
        img.src = 'imagens/manha.jpg'
+       document.body.style.backgroundColor = 'rgb(0,128,255)'
     } else if (hora >=12 && hora < 18) {
         // boa tarde
         img.src = 'imagens/tarde.jpg'
+        document.body.style.backgroundColor = 'rgb(255,128,0)'
     } else {
         // boa noite
         img.src = 'imagens/noite.jpg'
+        document.body.style.backgroundColor = 'rgb(0,0,0)'
     }
 }
 
---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+// estrutura de repetição com teste lógico no início
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+// estrutura de repetição com teste lógico no final
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+// estrutura de repetição burra pra caralho
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
